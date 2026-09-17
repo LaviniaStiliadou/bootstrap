9570200651b66d61f4cca8987820c82e940bb0de
LaviniaStiliadou <livia_16@live.de>
2026-09-17 22:52:19 +0200

    change docsa

--- site/astro.config.ts
+++ site/astro.config.ts
@@ -8,19 +8,18 @@ import { stackblitzPlugin } from './src/plugins/stackblitz-plugin'
 const isDev = process.env.NODE_ENV === 'development'
 
 const site = isDev
-  ? // In development mode, use the local dev server.
-    'http://localhost:9001'
-  : process.env.DEPLOY_PRIME_URL !== undefined
-    ? // If deploying on Netlify, use the `DEPLOY_PRIME_URL` environment variable.
-      process.env.DEPLOY_PRIME_URL
-    : // Otherwise, use the `baseURL` value defined in the `config.yml` file.
-      getConfig().baseURL
+  ? 'http://localhost:9001'
+  : process.env.GITHUB_PAGES_URL ??
+    getConfig().baseURL
 
-// https://astro.build/config
 export default defineConfig({
+  site,
+  base: new URL(site).pathname,
+
   build: {
     assets: `docs/${getConfig().docs_version}/assets`
   },
+
   integrations: [
     bootstrap(),
     astroBrokenLinksChecker({
@@ -30,11 +29,12 @@ export default defineConfig({
       linkCheckerDir: '.link-checker'
     })
   ],
+
   markdown: {
     smartypants: false,
     syntaxHighlight: 'prism'
   },
-  site,
+
   vite: {
     plugins: [algoliaPlugin(), stackblitzPlugin()]
   }
